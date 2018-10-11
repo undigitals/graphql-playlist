c04b56be521a80590b11cdd1bdda25e23c024fac
Umid Negmatullayev <undigitals@gmail.com>
2018-10-12 04:26:31 +0900

    changed id's from  GraphQLString to GraphQLID

--- schema/schema.js
+++ schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 // ES 6 Destructuring
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID} = graphql;
 
 // Dummy data
 var books = [
@@ -14,7 +14,7 @@ var books = [
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: { type: GraphQLString },
+        id: { type: GraphQLID },
         name: { type: GraphQLString},
         genre: { type:GraphQLString }
     })
@@ -25,9 +25,11 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: {id: {type: GraphQLString }},
+            args: {id: {type: GraphQLID }},
             resolve(parent, args){
                 // code to get data from db / other source
+                //checking if the ID is string or not
+                //console.log(typeof(args.id));
                 return _.find(books, {id: args.id});
             }
         }
